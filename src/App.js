3492e1bb1aef2eae09fa093933305fa6d89c02d2
ThaiBuiEverfit <thaibui@everfit.io>
2023-06-28 00:41:02 +0700

    admin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Resetpassword from "./pages/Resetpassword";
+import Forgotpassword from "./pages/Forgotpassword";
+import MainLayout from "./components/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Customers from "./pages/Customers";
+import Addproduct from "./pages/Addproduct";
+import Productlist from "./pages/Productlist";
+import Addbrand from "./pages/Addbrand";
+import Brandlist from "./pages/Brandlist";
+import Addcat from "./pages/Addcat";
+import Categorylist from "./pages/Categorylist";
+import Addcolor from "./pages/Addcolor";
+import Colorlist from "./pages/Colorlist";
+import Orders from "./pages/Orders";
+import Addblog from "./pages/Addblog";
+import Bloglist from "./pages/Bloglist";
+import Addblogcat from "./pages/Addblogcat";
+import Blogcatlist from "./pages/Blogcatlist";
+import Enquiries from "./pages/Enquiries";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reset-password" element={<Resetpassword />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
+        <Route path="/admin" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="product" element={<Addproduct />} />
+          <Route path="list-product" element={<Productlist />} />
+          <Route path="brand" element={<Addbrand />} />
+          <Route path="list-brand" element={<Brandlist />} />
+          <Route path="category" element={<Addcat />} />
+          <Route path="list-category" element={<Categorylist />} />
+          <Route path="color" element={<Addcolor />} />
+          <Route path="list-color" element={<Colorlist />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="blog" element={<Addblog />} />
+          <Route path="blog-list" element={<Bloglist />} />
+          <Route path="blog-category" element={<Addblogcat />} />
+          <Route path="blog-category-list" element={<Blogcatlist />} />
+          <Route path="enquiries" element={<Enquiries />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/CustomInput.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CustomInput = (props) => {
+  const { type, label, i_id, i_class } = props;
+
+  return (
+    <div className="form-floating mb-3">
+      <input
+        type={type}
+        className={`form-control ${i_class}`}
+        id={i_id}
+        placeholder={label}
+      />
+      <label htmlFor={label}>{label}</label>
+    </div>
+  );
+};
+
+export default CustomInput;
+
+CustomInput.propTypes = {
+  type: PropTypes.string,
+  label: PropTypes.string,
+};
